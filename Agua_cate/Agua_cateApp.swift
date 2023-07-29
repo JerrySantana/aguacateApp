d0f1e20764b3bc1403a3410323533bb593dc5aa6
Gerardo Santana <gs.amezcua@comunidad.unam.mx>
2023-07-28 21:35:08 -0500

    Initial Commit

---
+++ Agua_cate/Agua_cateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agua_cateApp.swift
+//  Agua_cate
+//
+//  Created by user246216 on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Agua_cateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
